c05cc82796083881450ba79b447ee02b8608c402
thomas.bradac <thomas.bradac@MU-C1-10.mullen.pvt.k12.co.us>
2026-02-11 14:09:38 -0700

    Welcome message

--- SIS Project/src/SISRunner.java
+++ SIS Project/src/SISRunner.java
@@ -4,7 +4,7 @@ public class SISRunner
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
+				//Welcome to group project - Thomas,JT, Julia, Feliciana.
 
 			}
 
