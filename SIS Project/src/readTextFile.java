377693868d3786990a00775dee3608cd1ad5552d
thomas.bradac <thomas.bradac@MU-C1-10.mullen.pvt.k12.co.us>
2026-02-18 14:59:20 -0700

    working on reading text file. not complete yet.

--- SIS Project/src/readTextFile.java
+++ SIS Project/src/readTextFile.java
@@ -1,10 +1,25 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
-public class readTextFile
+public class readTextFile 
 	{
-		String fileName = "StudentInfo.txt";
-		Scanner fileReader = new Scanner(new File("StudentInfo.txt"));
-		// need to complete reading text file.
+		
+		public static void readTextFile() throws IOException
+		{
+			String fileName = "StudentInfo.txt";
+			Scanner fileReader = new Scanner(new File("StudentInfo.txt"));
+			while (fileReader.hasNextLine()) 
+			{
+				String data = fileReader.nextLine();
+				String[] dataSplit = data.split(" ");
+			}
+			ArrayList<String> students = new ArrayList<String>();
+			
+			
+			
+			
+		}
+		
 	}
