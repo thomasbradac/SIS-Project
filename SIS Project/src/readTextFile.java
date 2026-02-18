e76b0c019d6cd38cb7f04eab0e892dc28400a94e
thomas.bradac <thomas.bradac@MU-C1-10.mullen.pvt.k12.co.us>
2026-02-18 14:31:58 -0700

    Initial Text File Commit

---
+++ SIS Project/src/readTextFile.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class readTextFile
+	{
+		String fileName = "StudentInfo.txt";
+		Scanner fileReader = new Scanner(new File("StudentInfo.txt"));
+	}
