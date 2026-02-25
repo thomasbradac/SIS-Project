bf6e58a9b229ed4632bd8d6ac41ee442e94f4d8d
thomas.bradac <thomas.bradac@MU-C1-10.mullen.pvt.k12.co.us>
2026-02-25 15:01:07 -0700

    almost completed filling arrayList. need to fix consturctor

--- SIS Project/src/readTextFile.java
+++ SIS Project/src/readTextFile.java
@@ -5,37 +5,133 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class readTextFile 
 	{
-		
+		static ArrayList<Student> students = new ArrayList<Student>();	
 		
 		
 		public static void readTextFile() throws IOException
 		{
-			ArrayList<Student> students = new ArrayList<Student>();
+			
 			String fileName = "StudentInfo.txt";
 			Scanner fileReader = new Scanner(new File("StudentInfo.txt"));
 			while (fileReader.hasNextLine()) 
 				{
 					String data = fileReader.nextLine();
 					String[] dataSplit = data.split(" ");
+					students.add(new Student(dataSplit[0], dataSplit[1] , gpaCalculator(dataSplit) , dataSplit[2] , dataSplit[3] , dataSplit[4] , dataSplit[5] , dataSplit[6] , dataSplit[7]));
 					
 				}
 			
 		}
 		
 		
-		public static int gpaCalculator (int class1Grade , int class2Grade , int class3Grade , String[] dataSplit)
+		public static double gpaCalculator (String[] dataSplit)
 			{
+				double gpa1 = 0;
+				double gpa2 = 0;
+				double gpa3 = 0;
+			
+				switch (dataSplit[3])
+				{
+					case "A+": gpa1 = 4.3;
+						
+					case "A": gpa1 = 4.0;
+						
+					case "A-": gpa1 = 3.7;
+						
+					case "B+": gpa1 = 3.3;
+					
+					case "B": gpa1 = 3.0;
+					
+					case "B-": gpa1 = 2.7;
+						 
+					case "C+": gpa1 = 2.3;
+					
+					case "C": gpa1 = 2.0;
+						
+					case "C-": gpa1 = 1.7;
+						
+					case "D+": gpa1 = 1.3;
+						
+					case "D": gpa1 = 1.0;
+						
+					case "D-": gpa1 = 0.7;
+						
+					case "F": gpa1 = 0.0;
+						
+					default:	
+				}
 				
+				switch (dataSplit[5])
+				{
+					case "A+": gpa2 = 4.3;
+						
+					case "A": gpa2 = 4.0;
+						
+					case "A-": gpa2 = 3.7;
+						
+					case "B+": gpa2 = 3.3;
+					
+					case "B": gpa2 = 3.0;
+					
+					case "B-": gpa2 = 2.7;
+						 
+					case "C+": gpa2 = 2.3;
+					
+					case "C": gpa2 = 2.0;
+						
+					case "C-": gpa2 = 1.7;
+						
+					case "D+": gpa2 = 1.3;
+						
+					case "D": gpa2 = 1.0;
+						
+					case "D-": gpa2 = 0.7;
+						
+					case "F": gpa2 = 0.0;
+						
+					default:	
+				}
 				
+				switch (dataSplit[7])
+				{
+					case "A+": gpa3 = 4.3;
+						
+					case "A": gpa3 = 4.0;
+						
+					case "A-": gpa3 = 3.7;
+						
+					case "B+": gpa3 = 3.3;
+					
+					case "B": gpa3 = 3.0;
+					
+					case "B-": gpa3 = 2.7;
+						 
+					case "C+": gpa3 = 2.3;
+					
+					case "C": gpa3 = 2.0;
+						
+					case "C-": gpa3 = 1.7;
+						
+					case "D+": gpa3 = 1.3;
+						
+					case "D": gpa3 = 1.0;
+						
+					case "D-": gpa3 = 0.7;
+						
+					case "F": gpa3 = 0.0;
+						
+					default:	
+				}
 				
+				double gpa = (gpa1 + gpa2 + gpa3)/(3);
 				
-				return 0; 
+				return gpa;
 			}
 		
 		
 		public static void fillArrayList()
 		{
-			//students.add(new Student(dataSplit[0], dataSplit[1] , Integer.parseInt(dataSplit[2]) , dataSplit[3] , dataSplit[4] , dataSplit[5] , dataSplit[6] , dataSplit[7] , dataSplit[8]));
+			//
 		}
 		
 	}
