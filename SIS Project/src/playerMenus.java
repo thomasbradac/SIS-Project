37c1646a5b68dfc97d4027539598112764980eb2
Felicianna.DeSantos <Felicianna.DeSantos@MU-C1-06.mullen.pvt.k12.co.us>
2026-02-25 14:59:22 -0700

    initial commit

---
+++ SIS Project/src/playerMenus.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class playerMenus
+	{
+		static Scanner userIntInput = new Scanner(System.in);
+		 public static void mainMenus()
+		 {
+			 System.out.println("Welcome!");
+			 System.out.println("What would you like to do?");
+			 System.out.println("1. Add or delete a student");
+			 System.out.println("2. Change student grades/schedule");
+			 System.out.println("3. Sort students");
+			 int choice = userIntInput.nextInt();
+			 
+			 if(choice == 1){
+					 
+	         }else if(choice == 2) {
+	        	 
+	         }else if(choice == 3) {
+	        	 
+	         }
+		 }
+
+	}
