cabecc4920606d46b3a1c95851bc90896ee685c3
Marc <Marc@MARC-LAPTOP>
2026-03-10 21:58:51 -0600

    Comparator complete, but the playerMenus class needs to add choice for user to pick which class period to sort by, then the comparator can be functional.

--- SIS Project/src/Sorting.java
+++ SIS Project/src/Sorting.java
@@ -1,15 +1,33 @@
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class Sorting implements Comparator<Student>
-	{
-		public int compare(Student s1, Student s2)
-			{
-int result = 0;
-return 0;
-			}
-		public static void sortClassroom()
+public class Sorting implements Comparator<Student> {
+	public int compare(Student s1, Student s2) {
+		int result = 0;
+		
+		if (playerMenus.Menus() == 1)
 		{
-			Collections.sort(students, new Sorting());
+			result = s1.getFirstName().compareToIgnoreCase(s2.getFirstName());
 		}
+		
+		else if (playerMenus.Menus() == 2)
+		{
+			result = Double.compare(s2.getGPA(), s1.getGPA());
+		}
+		
+		else if (playerMenus.Menus() == 3)
+		{
+			result = s1.getClass1().compareToIgnoreCase(s2.getClass1());
+		}
+
+		return result;
 	}
+
+	public static void sortClassroom() {
+		Collections.sort(readTextFile.students, new Sorting());
+	}
+}
